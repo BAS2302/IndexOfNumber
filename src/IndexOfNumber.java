ea12e6d8cddb6350ed2aedf4ebd91ccad522a17b
BelAnton <belanton@mail.ru>
2020-09-15 21:41:57 +0500

    made index and number

--- src/IndexOfNumber.java
+++ src/IndexOfNumber.java
@@ -1,5 +1,17 @@
 public class IndexOfNumber {
     public static void main(String[] args) {
         int[] array = {15, 34, 5, 3, -11, 567, 2, 547, 2, 0, -12, 35};
+        int number = 567;
+        int index = getFirstIndex(array, number);
+        System.out.println("Index of number in array = " + index);
+    }
+
+    public static int getFirstIndex(int[] array, int number) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
